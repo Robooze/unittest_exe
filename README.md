20f1b96fc2790cc739e065060101fc07743312d2
Rob <rob.genchi@gmail.com>
2019-09-17 17:26:57 +0200

    Polished functions, tests and README

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
-# unittest_exe
+# Unit Tests with Python
 Trying out __unittest__ and __git__ implementation on Pycharm.
 
 To-do list:
-- [x]   
+1. [x] Create a new repository and import it on PyCharm
+2. [x] Branch out of master
+3. [x] Create a library of functions to test
+4. [x] Create the tests
+5. [x] Add-commit-push to remote
+6. [ ] Create a Pull Request on GitHub
+
+This is the approach:
+* We create single tests under a child-class of `unittest.TestCase`. The tests compare two propositions, if the comparision isn't `True` the test is failed.
+* We can have a `setUp(self)` method that is initialized every time before a test is launched.
--- functions.py
+++ functions.py
@@ -1,31 +1,40 @@
+"""
+This is gonna be a library of functions to test in the Unit Test
+"""
+
+
 def gen_to_list(num):
     return list(range(num))
 
 
 def division(a, b):
-    return a / b
+    try:
+        return a / b
+    except ZeroDivisionError as e:
+        print("Cannot divide by 0!")
 
 
 def int_division(a, b):
-    return a // b
-
-
-'''
-{
-a=4
-b=2
-}
-
-result:
-{
-a=4
-b=2
-r=2
-}
-'''
+    try:
+        return a // b
+    except ZeroDivisionError as e:
+        print("Cannot divide by 0!")
 
 
 def dict_division(**dizionario):
+    '''
+    {
+    a=4
+    b=2
+    }
+
+    result:
+    {
+    a=4
+    b=2
+    r=2
+    }
+    '''
     a = dizionario["a"]
     b = dizionario["b"]
     return {"a": a, "b": b, "r": division(a, b)}
